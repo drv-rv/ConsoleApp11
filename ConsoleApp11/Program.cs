d48abeffb9f72e4451d5cd1b27af8f3426886d44
drv-rv <60234556+drv-rv@users.noreply.github.com>
2020-01-24 13:04:57 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 39, 2, 9, 4, 555, 87, 5, 8 };
+
+            int max = int.MinValue;
+            int index = 0;
+            int min = int.MaxValue;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] > max)
+                {
+
+                    max = a[i];
+                    index = i;
+                }
+
+                if (a[i] < min)
+                {
+
+                    min = a[i];
+
+                }
+            }
+            Console.WriteLine(index);
+            Console.WriteLine(min);
+        }
+    }
+}
